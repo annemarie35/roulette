9d8526598319725beac11e664294c4115f0110a5
walidoAtlas <w.yahiacherif@gmail.com>
2020-03-27 10:51:32 +0100

    writing tests for the creation of random pairs

--- roulette/roulette.js
+++ roulette/roulette.js
@@ -8,8 +8,6 @@ export const pairGenerator = (peopleList) => {
       }
   })
 
-
-
   return cleanPeopleList
 }
 
--- roulette/roulette.spec.js
+++ roulette/roulette.spec.js
@@ -50,8 +50,43 @@ describe('pairGenerator', () => {
     })
   })
 
-  describe('when array contains most than 4 peopleNames', ()=>{
-    test('should return random pairs of peopleNames', ()=>{
+  describe.skip('when array contains less than 4 peopleName', () => {
+    test('should return the given list for 1 peopleName', () => {
+      // given
+      const cleanPeopleList = ['DGY']
+
+      // when 
+      const result = generatePairs(cleanPeopleList)
+
+      // then
+      expect(result.length).toEqual(1)
+    })
+
+    test('should return the given list for 2 peopleNames', () => {
+      // given
+      const cleanPeopleList = ['DGY', 'TRD']
+
+      // when 
+      const result = generatePairs(cleanPeopleList)
+
+      // then
+      expect(result.length).toEqual(2)
+    })
+
+    test('should return the given listfor 3 peopleNames', () => {
+      // given
+      const cleanPeopleList = ['DGY', 'TRD','FGT']
+
+      // when 
+      const result = generatePairs(cleanPeopleList)
+
+      // then
+      expect(result.length).toEqual(3)
+    })
+  })
+  
+  describe('when array contains 4 peopleNames', () => {
+    test('should return random pairs of peopleNames', () => {
       // given
       const cleanPeopleList = ['DGY', 'TRD', 'FGT','MMG']
 
@@ -60,8 +95,132 @@ describe('pairGenerator', () => {
 
       // then
       expect(result.length).toEqual(2)
+      expect(result[0].length).toEqual(2)
+      expect(result[1].length).toEqual(2)
+    })
+  })
+  
+  describe.skip('when array contains 5 peopleNames', () => {
+    test('should return random a group of peopleNames', () => {
+      // given
+      const cleanPeopleList = ['DGY', 'TRD', 'FGT','MMG','RTD']
 
+      // when 
+      const result = generatePairs(cleanPeopleList)
+
+      // then
+      expect(result.length).toEqual(1)
+      expect(result[0].length).toEqual(5)
     })
   })
 
+  describe.skip('when array contains 6 peopleNames', () => {
+    test('should return random pairs of peopleNames', () => {
+      // given
+      const cleanPeopleList = ['DGY', 'TRD', 'FGT','MMG','RTD','TGHS']
+
+      // when 
+      const result = generatePairs(cleanPeopleList)
+
+      // then
+      expect(result.length).toEqual(1)
+      expect(result[0].length).toEqual(2)
+      expect(result[1].length).toEqual(2)
+      expect(result[2].length).toEqual(2)
+    })
+  })
+  
+  describe.skip('when array contains 7 peopleNames', () => {
+    test('should return random pairs of peopleNames', () => {
+      // given
+      const cleanPeopleList = ['DGY', 'TRD', 'FGT','MMG','RTD','TGHS','CDT']
+
+      // when 
+      const result = generatePairs(cleanPeopleList)
+
+      // then
+      expect(result.length).toEqual(1)
+      expect(result[0].length).toEqual(2)
+      expect(result[1].length).toEqual(2)
+      expect(result[2].length).toEqual(3)
+    })
+  })
+
+  describe.skip('when array contains 8 peopleNames', () => {
+    test('should return random pairs of peopleNames', () => {
+      // given
+      const cleanPeopleList = ['DGY', 'TRD', 'FGT','MMG','RTD','TGHS','CDT','MLK']
+
+      // when 
+      const result = generatePairs(cleanPeopleList)
+
+      // then
+      expect(result.length).toEqual(1)
+      expect(result[0].length).toEqual(4)
+      expect(result[1].length).toEqual(4)
+    })
+  })
+  
+  describe.skip('when array contains 9 peopleNames', () => {
+    test('should return random pairs of peopleNames', () => {
+      // given
+      const cleanPeopleList = ['DGY', 'TRD', 'FGT','MMG','RTD','TGHS','CDT','MLK','BOSS',]
+
+      // when 
+      const result = generatePairs(cleanPeopleList)
+
+      // then
+      expect(result.length).toEqual(1)
+      expect(result[0].length).toEqual(4)
+      expect(result[1].length).toEqual(5)
+    })
+  })
+  
+  describe.skip('when array contains 10 peopleNames', () => {
+    test('should return random pairs of peopleNames', () => {
+      // given
+      const cleanPeopleList = ['DGY', 'TRD', 'FGT','MMG','RTD','TGHS','CDT','MLK','BOSS','PKJK']
+
+      // when 
+      const result = generatePairs(cleanPeopleList)
+
+      // then
+      expect(result.length).toEqual(1)
+      expect(result[0].length).toEqual(4)
+      expect(result[1].length).toEqual(4)
+      expect(result[1].length).toEqual(2)
+    })
+  })
+  
+  describe.skip('when array contains 11 peopleNames', () => {
+    test('should return random pairs of peopleNames', () => {
+      // given
+      const cleanPeopleList = ['DGY', 'TRD', 'FGT','MMG','RTD','TGHS','CDT','MLK','BOSS','PKJK','XDC']
+
+      // when 
+      const result = generatePairs(cleanPeopleList)
+
+      // then
+      expect(result.length).toEqual(1)
+      expect(result[0].length).toEqual(4)
+      expect(result[1].length).toEqual(4)
+      expect(result[1].length).toEqual(3)
+    })
+  })
+  
+  describe.skip('when array contains 12 peopleNames', () => {
+    test('should return random pairs of peopleNames', () => {
+      // given
+      const cleanPeopleList = ['DGY', 'TRD', 'FGT','MMG','RTD','TGHS','CDT','MLK','BOSS','PKJK','XDC','NBV']
+
+      // when 
+      const result = generatePairs(cleanPeopleList)
+
+      // then
+      expect(result.length).toEqual(1)
+      expect(result[0].length).toEqual(4)
+      expect(result[1].length).toEqual(4)
+      expect(result[1].length).toEqual(4)
+    })
+  })
 })
